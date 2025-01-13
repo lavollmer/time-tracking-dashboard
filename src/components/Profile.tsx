0109594c078f7b692d24b697a9f36fc8d511a0b1
Laura <laura.e.vollmer@gmail.com>
2025-01-13 11:43:10 -0600

    created the profile report card

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,7 +1,22 @@
+import ProfileImage from "../assets/image-jeremy.png";
+
 const Profile = () => {
   return (
-    <div>Profile</div>
-  )
-}
+    <div>
+      <div>
+        <img src={ProfileImage} alt="Profile Image" />
+      </div>
+      <div>
+        <h1>Report for</h1>
+        <h2>Jeremy Robson</h2>
+      </div>
+      <div>
+        <h3>Daily</h3>
+        <h3>Weekly</h3>
+        <h3>Monthly</h3>
+      </div>
+    </div>
+  );
+};
 
-export default Profile
+export default Profile;
