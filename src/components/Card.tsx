c906b888fa0ca1ff0ab370d17f85d41d1f42b490
Laura <laura.e.vollmer@gmail.com>
2025-01-15 12:34:15 -0600

    debugging the image layering and positioning in the card component

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -11,21 +11,24 @@ interface CardProps {
 const Card: React.FC<CardProps> = ({ title, hours, total, image, color }) => {
   return (
     <>
-      <div className="flex flex-col">
-        <div className="rounded-lg p-6 z-0" style={{ backgroundColor: color }}>
-          <img src={image} alt="icon-work" className="h-2/3 w-1/3" />
-        </div>
-        <div className="text-white bg-darkBlue font-rubik p-6 rounded-lg z-10">
-          <div>
-            <div className="flex flex-row justify-between items-center">
-              <h1 className="font-medium text-lg">{title}</h1>
-              <button>
-                <img src={iconDots} alt="icon-dots" />
-              </button>
-            </div>
-            <div className="flex flex-row justify-between items-center space-x-6">
-              <h2 className="font-regular text-4xl">{hours}</h2>
-              <h3 className="font-light text-sm text-paleBlue">{total}</h3>
+      <div className="relative">
+        <div
+          className="static rounded-lg z-10"
+          style={{ backgroundColor: color }}
+        >
+          <img src={image} alt="icon-work" className="p-1 z-0" />
+          <div className="text-white bg-darkBlue font-rubik p-6 rounded-lg z-20">
+            <div>
+              <div className="flex flex-row justify-between items-center">
+                <h1 className="font-medium text-lg">{title}</h1>
+                <button>
+                  <img src={iconDots} alt="icon-dots" />
+                </button>
+              </div>
+              <div className="flex flex-row justify-between items-center space-x-6">
+                <h2 className="font-regular text-4xl">{hours}</h2>
+                <h3 className="font-light text-sm text-paleBlue">{total}</h3>
+              </div>
             </div>
           </div>
         </div>
