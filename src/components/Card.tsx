fc6fc1c7c66bf6478b224cd87cec6adbaa127133
Laura <laura.e.vollmer@gmail.com>
2025-01-15 12:05:43 -0600

    working on the sections of the page

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,7 +1,20 @@
-const Card = () => {
-  return (
-    <div>Card</div>
-  )
-}
+import {iconDots} from "../assets/icon-ellipsis.svg"
 
-export default Card
+const Card = ({title,hours,total}) => {
+  return (
+    <div>
+      <div>
+        <div>
+          <h1>{title}</h1>
+          <button>
+            <img src={iconDots} alt="icon-dots" />
+          </button>
+        </div>
+        <h2>{hours}</h2>
+        <h3>{total}</h3>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
