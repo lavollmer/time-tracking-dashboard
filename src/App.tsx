4afeffe5248e32493637f1185a38d4f558892ade
Laura <laura.e.vollmer@gmail.com>
2025-01-15 11:44:29 -0600

    updating the background of the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import "./index.css";
+import "./App.css";
 import Card from "./components/Card";
 import Profile from "./components/Profile";
 
 function App() {
   return (
     <>
-      <div className="bg-blue">
-        <Profile/>
-        <Card/>
+      <div className="container-bg">
+        <Profile />
+        <Card />
       </div>
     </>
   );
