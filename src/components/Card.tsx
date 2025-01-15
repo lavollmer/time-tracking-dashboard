fd459216193868a7af9d90ffccd4bc999e7fb2e3
Laura <laura.e.vollmer@gmail.com>
2025-01-15 12:43:40 -0600

    debugging the overlap of the two containers for the card component

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -13,22 +13,22 @@ const Card: React.FC<CardProps> = ({ title, hours, total, image, color }) => {
     <>
       <div className="relative">
         <div
-          className="static rounded-lg z-10"
+          className="rounded-lg z-0 flex justify-end items-start"
           style={{ backgroundColor: color }}
         >
-          <img src={image} alt="icon-work" className="p-1 z-0" />
-          <div className="text-white bg-darkBlue font-rubik p-6 rounded-lg z-20">
-            <div>
-              <div className="flex flex-row justify-between items-center">
-                <h1 className="font-medium text-lg">{title}</h1>
-                <button>
-                  <img src={iconDots} alt="icon-dots" />
-                </button>
-              </div>
-              <div className="flex flex-row justify-between items-center space-x-6">
-                <h2 className="font-regular text-4xl">{hours}</h2>
-                <h3 className="font-light text-sm text-paleBlue">{total}</h3>
-              </div>
+          <img src={image} alt="icon-work" className="p-2" />
+        </div>
+        <div className="-mt-16 w-full text-white bg-darkBlue font-rubik p-6 rounded-lg z-20">
+          <div>
+            <div className="flex flex-row justify-between items-center">
+              <h1 className="font-medium text-lg">{title}</h1>
+              <button>
+                <img src={iconDots} alt="icon-dots" />
+              </button>
+            </div>
+            <div className="flex flex-row justify-between items-center space-x-6">
+              <h2 className="font-regular text-4xl">{hours}</h2>
+              <h3 className="font-light text-sm text-paleBlue">{total}</h3>
             </div>
           </div>
         </div>
