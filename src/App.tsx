3de78f822df05600f703e2b8f77e3ad359dd1b00
Laura <laura.e.vollmer@gmail.com>
2025-01-16 16:48:57 -0600

    updating the app tsx file for formatting

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,9 @@ import Profile from "./components/Profile";
 function App() {
   return (
     <>
-      <div className="container-bg">
+      <div className="flex flex-row items-center space-x-4 container-bg">
         <div className="grid grid-2-cols flex flex-row"></div>
-        <div>
+        <div className="flex flex-col">
           <Profile />
         </div>
         <div>
