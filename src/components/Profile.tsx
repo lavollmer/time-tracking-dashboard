6dd406f6cc4e376aa43ba25f0094d9d095155060
Laura <laura.e.vollmer@gmail.com>
2025-01-17 11:50:04 -0600

    updating the navigation section

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,46 +1,57 @@
 import ProfileImage from "../assets/image-jeremy.png";
 import "../index.css";
 import "../App.css";
-import {Navigate, useNavigate} from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const Profile = () => {
+  const navigate = useNavigate();
 
   const clickDaily = () => {
     console.log("Daily Clicked");
-    Navigate("/daily");
+    navigate("/daily");
   };
 
   const clickWeekly = () => {
     console.log("Weekly Clicked");
-  }
+    navigate("/weekly");
+  };
 
   const clickMonthly = () => {
     console.log("Monthly Clicked");
-  }
+    navigate("/monthly");
+  };
 
   return (
-    <div className="flex flex-col space-y-6 bg-blue text-white font-rubik p-4 rounded-lg">
-      <div className="rounded-full">
-        <div>
-          <img
-            src={ProfileImage}
-            alt="Profile Image"
-            className="border border-white rounded-full ring w-16 h-16"
-          />
+    <>
+      <div className="flex flex-col space-y-6 bg-blue text-white font-rubik p-4 rounded-lg">
+        <div className="rounded-full">
+          <div>
+            <img
+              src={ProfileImage}
+              alt="Profile Image"
+              className="border border-white rounded-full ring w-16 h-16"
+            />
+          </div>
+          <div className="text-left  pt-4">
+            <h1 className="text-sm font-light">Report for</h1>
+            <h2 className="text-4xl font-regular">Jeremy Robson</h2>
+          </div>
         </div>
-        <div className="text-left  pt-4">
-          <h1 className="text-sm font-light">Report for</h1>
-          <h2 className="text-4xl font-regular">Jeremy Robson</h2>
+        <div className="bg-darkBlue w-full h-full p-4 rounded-lg">
+          <div className="flex flex-col space-y-2">
+            <button className="hover:font-bold" onClick={clickDaily}>
+              Daily
+            </button>
+            <button className="hover:font-bold" onClick={clickWeekly}>
+              Weekly
+            </button>
+            <button className="hover:font-bold" onClick={clickMonthly}>
+              Monthly
+            </button>
+          </div>
         </div>
       </div>
-      <div className="bg-darkBlue w-full h-full p-4 rounded-lg">
-        <div className="flex flex-col space-y-2">
-          <button className="hover:font-bold" onClick={clickDaily}>Daily</button>
-          <button className="hover:font-bold" onClick={clickWeekly}>Weekly</button>
-          <button className="hover:font-bold" onClick={clickMonthly}>Monthly</button>
-        </div>
-      </div>
-    </div>
+    </>
   );
 };
 
