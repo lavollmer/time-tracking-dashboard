bf728d2a4938b29c2c266119920b89a192b4c9ca
Laura <laura.e.vollmer@gmail.com>
2025-01-17 11:52:17 -0600

    updating the app react routing

--- src/App.tsx
+++ src/App.tsx
@@ -4,27 +4,19 @@ import Weekly from "./components/Weekly";
 import Profile from "./components/Profile";
 import Daily from "./components/Daily";
 import Monthly from "./components/Monthly";
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <>
-    <BrowserRouter>
-    <Routes>
-      <Route path='/daily' element={<Daily />} />
-      <Route path='/weekly' element={<Weekly />} />
-      <Route path='/monthly' element={<Monthly />} />
-    </Routes>
-    </BrowserRouter>
-      <div className="flex flex-row items-center space-x-4 container-bg">
-        <div className="grid grid-2-cols flex flex-row"></div>
-        <div className="flex flex-col">
-          <Profile />
-        </div>
-        <div>
-          <Weekly />
-        </div>
-      </div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Profile />} />
+          <Route path="/daily" element={<Daily />} />
+          <Route path="/weekly" element={<Weekly />} />
+          <Route path="/monthly" element={<Monthly />} />
+        </Routes>
+      </Router>
     </>
   );
 }
