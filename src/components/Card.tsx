ccf62b4d4450dc278e092ecd886c94a5513f5b51
Laura <laura.e.vollmer@gmail.com>
2025-01-21 15:12:40 -0600

    updating styling based on the card design file

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -12,25 +12,24 @@ interface CardProps {
 const Card: React.FC<CardProps> = ({ title, hours, total, image, color }) => {
   return (
     <>
-      <div className="relative w-full h-24">
-        <div className="rounded-lg flex w-full" style={{ backgroundColor: color }}>
-          <img
-            src={image}
-            alt="icon-work"
-            className="p-2 h-16 w-16"
-          />
+      <div className="relative w-full h-24 cursor-pointer rounded-lg">
+        <div
+          className="rounded-lg flex w-full"
+          style={{ backgroundColor: color }}
+        >
+          <img src={image} alt="icon-work" className="p-2 h-16 w-16" />
         </div>
-        <div className="absolute -mt-16 w-full text-white bg-darkBlue font-rubik p-6 rounded-lg z-20">
-          <div>
+        <div className="absolute -mt-16 w-full text-white bg-darkBlue hover:bg-desaturatedBlue font-rubik p-6 rounded-lg z-20">
+          <div className="flex flex-col space-y-2">
             <div className="flex flex-row justify-between items-center">
               <h1 className="font-medium text-lg">{title}</h1>
               <button>
                 <img src={iconDots} alt="icon-dots" />
               </button>
             </div>
-            <div className="flex flex-row justify-between items-center space-x-6">
+            <div className="flex flex-col text-left space-y-2">
               <h2 className="font-light text-4xl">{hours}</h2>
-              <h3 className="font-light text-sm text-paleBlue">{total}</h3>
+              <h2 className="font-light text-sm text-paleBlue">{total}</h2>
             </div>
           </div>
         </div>
